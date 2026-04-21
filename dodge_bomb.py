64d1dbdc2f3daad56cc3e12187e010f46099b4a8
c0a25018 <c0a25018be@edu.teu.ac.jp>
2026-04-21 14:23:19 +0900

    練習２：爆弾実装

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,7 +1,7 @@
 import os
 import sys
 import pygame as pg
-
+import random
 
 WIDTH, HEIGHT = 1100, 650
 DELTA = {pg.K_UP: (0, -5), pg.K_DOWN: (0, +5), pg.K_LEFT: (-5, 0), pg.K_RIGHT: (5, 0),}
@@ -15,6 +15,14 @@ def main():
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
+
+    bb_img = pg.Surface((20, 20)) # 爆弾用の空のSurfaceを作る 
+    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10) # 爆弾円を描く 
+    bb_img.set_colorkey((0, 0, 0))
+    bb_rct = bb_img.get_rect() # 爆弾のRectを取得する
+    bb_rct.centerx = random.randint(0, WIDTH) # 爆弾の初期横座標を設定する
+    bb_rct.centery = random.randint(0, HEIGHT) # 爆弾の初期縦座標を設定する    
+    vx, vy = +5, +5 # 爆弾の速度
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -29,9 +37,11 @@ def main():
             if key_lst[key]: 
                 sum_mv[0] += mv[0]
                 sum_mv[1] += mv[1]
-                
+
         kk_rct.move_ip(sum_mv)
         screen.blit(kk_img, kk_rct)
+        bb_rct.move_ip(vx, vy)
+        screen.blit(bb_img, bb_rct)
         pg.display.update()
         tmr += 1
         clock.tick(50)
